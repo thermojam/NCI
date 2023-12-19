73fe9bc9c2590a93d7a4e51bbc4769c93f32039d
Nikita Mensky <nmensky@gmail.com>
2023-12-19 15:46:29 +0300

    comm test link overlay

--- js/reviews.js
+++ js/reviews.js
@@ -28,21 +28,21 @@ let swiper = new Swiper(".mySwiper", {
         },
     },
     pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-        clickable: true,
+        el: '.swiper-pagination', // Селектор контейнера для пагинации
+        clickable: true, // Сделать пагинацию кликабельной
     },
 });
 
 //click slide links
-document.querySelectorAll('.swiper-slide').forEach(slide => {
-    const overlay = slide.querySelector('.overlay');
-    const link = overlay.querySelector('a');
+// document.querySelectorAll('.swiper-slide').forEach(slide => {
+//     const overlay = slide.querySelector('.overlay');
+//     const link = overlay.querySelector('a');
+//
+//     overlay.addEventListener('click', () => {
+//         const url = link.getAttribute('href');
+//         if (url) {
+//             window.open(url, '_blank');
+//         }
+//     });
+// });
 
-    overlay.addEventListener('click', () => {
-        const url = link.getAttribute('href');
-        if (url) {
-            window.open(url, '_blank');
-        }
-    });
-});
