7368fb698bc9d7a0b148626f75edc4a6ada2475c
Nikita Mensky <nmensky@gmail.com>
2024-01-14 22:23:37 +0300

    adaptive version, not Catalog

--- js/main.js
+++ js/main.js
@@ -1,36 +1,75 @@
-//slider-catalog
-let swiper1 = new Swiper('#swiper1', {
+// REVIEW-SLIDER
+let swiper5 = new Swiper("#swiper5", {
     slidesPerView: 3,
-    spaceBetween: 20,
-    grabCursor: true,
-    lazy: true,
-});
+    spaceBetween: 60,
 
-// Инициализация Swiper 2
-let swiper2 = new Swiper('#swiper2', {
-    slidesPerView: 3,
-    spaceBetween: 20,
+    loop: true,
+
     grabCursor: true,
-    lazy: true,
-    stopOnInteraction: false,
-    autoplay: {
-        delay: 5000,
+    breakpoints: {
+        1025: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        345: {
+            slidesPerView: 1,
+        },
+        320: {
+            slidesPerView: 1,
+        },
     },
-
     pagination: {
-        el: ".swiper-pagination-catalog",
+        el: ".swiper-pagination-review",
         dynamicBullets: true,
         clickable: true,
     },
 });
 
-// Связывание слайдеров через общую пагинацию
-swiper1.controller.control = swiper2;
-swiper2.controller.control = swiper1;
+//click slide links
+document.querySelectorAll('#swiper5 .swiper-slide').forEach(slide => {
+    const overlay = slide.querySelector('.overlay');
+    const link = overlay.querySelector('a');
 
-//click whats-up
+    overlay.addEventListener('click', () => {
+        const url = link.getAttribute('href');
+        if (url) {
+            window.open(url, '_blank');
+        }
+    });
+});
+
+// SCREEN-SLIDER
+let swiper6 = new Swiper("#swiper6", {
+    slidesPerView: 3,
+    spaceBetween: 60,
+
+    loop: true,
+
+    grabCursor: true,
+    breakpoints: {
+        1025: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        345: {
+            slidesPerView: 1,
+        },
+        320: {
+            slidesPerView: 1,
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination-screen",
+        dynamicBullets: true,
+        clickable: true,
+    },
+});
+
+//Click Whats-up
 function redirectToWhatsApp() {
     window.open('https://api.whatsapp.com/send?phone=79215939333', '_blank');
 }
-
-
