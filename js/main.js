1a32c6dde00482c00cde1a03bed2eb6e808bde92
Nikita Mensky <nmensky@gmail.com>
2024-01-25 12:47:28 +0300

    remastering review section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+// REVIEW-SLIDER
+let swiper5 = new Swiper("#swiper5", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+
+    loop: true,
+
+    grabCursor: true,
+    breakpoints: {
+        1025: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        345: {
+            slidesPerView: 1,
+        },
+        320: {
+            slidesPerView: 1,
+        },
+        300: {
+            slidesPerView: 1,
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination-review",
+        dynamicBullets: true,
+        clickable: true,
+    },
+});
+
+//click slide links
+document.querySelectorAll('#swiper5 .swiper-slide').forEach(slide => {
+    const overlay = slide.querySelector('.overlay');
+    const link = overlay.querySelector('a');
+
+    overlay.addEventListener('click', () => {
+        const url = link.getAttribute('href');
+        if (url) {
+            window.open(url, '_blank');
+        }
+    });
+});
 
 // SCREEN-SLIDER
 let swiper6 = new Swiper("#swiper6", {
