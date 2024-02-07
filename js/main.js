5334a4e19628067bb7134a1ad94b35df32e2ad08
Nikita Mensky <nmensky@gmail.com>
2024-02-07 19:14:16 +0300

    remaster main.js catalog and header

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 // NAV-LINKS-SHOW
 document.querySelectorAll('.nav-link').forEach(item => {
     item.addEventListener('click', () => {
-        document.querySelector('.navbar-toggler').click(); // Симулируем клик по кнопке "бургер"
+        // Проверяем, является ли ширина окна меньше или равной 768px (предположим, что это ваш порог для мобильной версии)
+        if (window.innerWidth <= 992) {
+            const navbarToggler = document.querySelector('.navbar-toggler');
+
+            // Проверяем, свернуто ли меню
+            if (!navbarToggler.classList.contains('collapsed')) {
+                navbarToggler.click(); // Симулируем клик по кнопке "бургер"
+            }
+        }
     });
 });
+
 
 //CATALOG-SLIDER-1
 let swiper1 = new Swiper('#swiper1', {
@@ -72,38 +81,54 @@ let swiper2 = new Swiper('#swiper2', {
 swiper1.controller.control = swiper2;
 swiper2.controller.control = swiper1;
 
-//CATALOG-SLIDER-3
-function initSwipers(containerSelector) {
-    let containers = document.querySelectorAll(containerSelector);
 
-    containers.forEach(function (container) {
-        let mainSlider = new Swiper(container.querySelector('.main-slider'), {
-            spaceBetween: 10,
-            allowTouchMove: false,
-            lazy: true,
-            autoplay: {
-                delay: 5000,
-            },
-        });
+// PRELOAD MODAL SLIDERS
+let modalIDs = ['modalBMW_520d', 'modalBMW_330_G20', 'modalBMW_X6_x_drive40i_M_Sport',
+    'modalMercedes-Benz_AMG_GT43', 'modalMercedes-Benz_V250d', 'modalBMW_X4_M40i_xDrive', 'modalMercedes-Benz_S500_4M',
+    'modalMERCEDES-BENZ_GLE_COUPE', 'modalBMW_740d_xDrive-M_Sport', 'modalLand_Rover_Defender', 'modalMercedes-Benz_GLE_300d',
+    'modalAudi_A6_All_road_45_TDI'];
 
-//CATALOG-SLIDER-4
-        let thumbnailSlider = new Swiper(container.querySelector('.thumbnail-slider'), {
-            slidesPerView: 5,
-            spaceBetween: 10,
-            lazy: true,
-        });
+// Инициализация обработчика для каждого модального окна
+modalIDs.forEach(function (modalID) {
+    $('#' + modalID).on('shown.bs.modal', function () {
 
-        mainSlider.controller.control = thumbnailSlider;
-        thumbnailSlider.controller.control = mainSlider;
+        //CATALOG-SLIDER-3
+        function initSwipers(containerSelector) {
+            let containers = document.querySelectorAll(containerSelector);
 
-        thumbnailSlider.on('click', function () {
-            let clickedIndex = thumbnailSlider.clickedIndex;
-            mainSlider.slideTo(clickedIndex);
-        });
+            containers.forEach(function (container) {
+                let mainSlider = new Swiper(container.querySelector('.main-slider'), {
+                    spaceBetween: 10,
+                    allowTouchMove: false,
+                    lazy: true,
+                    autoplay: {
+                        delay: 5000,
+                    },
+                });
+
+                //CATALOG-SLIDER-4
+                let thumbnailSlider = new Swiper(container.querySelector('.thumbnail-slider'), {
+                    slidesPerView: 5,
+                    spaceBetween: 10,
+                    lazy: true,
+                });
+
+                mainSlider.controller.control = thumbnailSlider;
+                thumbnailSlider.controller.control = mainSlider;
+
+                thumbnailSlider.on('click', function () {
+                    let clickedIndex = thumbnailSlider.clickedIndex;
+                    mainSlider.slideTo(clickedIndex);
+                });
+            });
+        }
+
+        initSwipers('.slider-container');
+
+        console.log('Модальное окно с ID ' + modalID + ' отображено.');
     });
-}
+});
 
-initSwipers('.slider-container');
 
 // REVIEW-SLIDER
 let swiper5 = new Swiper("#swiper5", {
@@ -150,6 +175,7 @@ document.querySelectorAll('#swiper5 .swiper-slide').forEach(slide => {
     });
 });
 
+
 // SCREEN-SLIDER
 let swiper6 = new Swiper("#swiper6", {
     slidesPerView: 3,
@@ -182,6 +208,7 @@ let swiper6 = new Swiper("#swiper6", {
     },
 });
 
+
 //Modal URL !!!
 $(document).ready(function () {
     // Список идентификаторов модальных окон
@@ -199,6 +226,7 @@ $(document).ready(function () {
         }
     });
 });
+
 
 //Click Whats-up
 function redirectToWhatsApp() {
